3dcaa8520ccc5ad3be7da65e8562e8de7bba918e
John Lee <drumwolf@Johns-MacBook-Pro-2.local>
2018-03-25 08:47:35 -0700

    set pay dates for next 10 pay cycles

--- src/app/components/spreadsheet/spreadsheet.component.ts
+++ src/app/components/spreadsheet/spreadsheet.component.ts
@@ -7,9 +7,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SpreadsheetComponent implements OnInit {
 
-  constructor() { }
+  payDates:any[];
+  baseDate:string;
+  dateCount:number;
+
+  constructor() {
+    this.payDates = [];
+    this.baseDate = '2016-05-20';
+    this.dateCount = 10;
+    this.setPayDates();
+  }
 
   ngOnInit() {
+    console.log(this.payDates)
+  }
+
+  setPayDates() {
+    let payDate = new Date( this.baseDate.split('-') );
+    const currentDate = new Date();
+    while (this.payDates.length !== this.dateCount) {
+      if (payDate > currentDate) {
+        this.payDates.push( this.dateString(payDate) );
+      }
+      payDate.setDate(payDate.getDate() + 14);
+    }
+  }
+
+  dateString(date) {
+    const year  = date.getFullYear(),
+        month = date.getMonth() + 1,
+        day   = date.getDate();
+    return `${year}-${(month < 10) ? '0'+month : month}-${(day < 10) ? '0'+day : day}`;
   }
 
 }
