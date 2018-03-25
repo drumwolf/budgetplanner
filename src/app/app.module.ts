7d438775534e333a84c534d1a02222d8757a34f9
John Lee <drumwolf@Johns-MacBook-Pro-2.local>
2018-03-24 20:07:39 -0700

    new component Spreadsheet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { SpreadsheetComponent } from './components/spreadsheet/spreadsheet.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SpreadsheetComponent
   ],
   imports: [
     BrowserModule
